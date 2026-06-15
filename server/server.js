c5371a7926db7b57d185e8742fba17b7e10d8b94
Rupali Banerjee <71378383+RupaliBanerjee@users.noreply.github.com>
2026-06-15 20:24:30 +0530

    Update backend server port

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.use("/mentor", mentorRoutes);
 
 
 /* Mongoose Setup */
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 //const PORT = 5000;
 //process.env.MONGO_URL
 mongoose
